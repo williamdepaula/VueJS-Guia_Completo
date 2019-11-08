50dfc5c8c4c62b1d559488a492f511ef4a760337
William de Paula <william.paula@azi.com.br>
2019-11-08 17:45:31 -0400

    Aula 94 - Padrão de nomes

---
+++ Secao-06/secao06-aula94/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <appContador/> 
+        <appHeader/>
+    </div>
+</template>
+
+<script>
+import AppContador from './AppContador'
+import AppHeader from './AppHeader'
+
+export default {
+    components: {
+        AppContador,
+        AppHeader
+    }    
+}
+</script>
---
+++ Secao-06/secao06-aula94/src/AppHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>
+        Curso de VueJS
+    </h1>
+</template>
